13386e06901b551c05c836630e4d415e8d1582a9
Mart Sõmermaa <mart.somermaa@cgi.com>
2014-05-18 21:52:47 +0300

    Rename C++11 detection define.

--- detect_cpp11.h
+++ detect_cpp11.h
@@ -2,7 +2,7 @@
 #define UTILCPP_DETECTCPP11_H__
 
 #if defined(__GXX_EXPERIMENTAL_CXX0X__) || (__cplusplus > 199711L) || defined(_MSC_VER) && _MSC_VER >= 1800
-  #define DBCCPP_HAVE_CPP11
+  #define UTILCPP_HAVE_CPP11
 #endif
 
 #endif
